60df14259c07bc4a9bf3c31a1a1b33dd7335ecd6
Colin Lienard <colin.lienard87@orange.fr>
2022-05-19 21:30:03 +0200

    :sparkles: Add the header and navbar of the home page in mobile

---
+++ components/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav class="nav">
+    <ul class="list">
+      <li>
+        <button class="link">Portfolio</button>
+      </li>
+      <li>
+        <button class="link">À propos</button>
+      </li>
+      <li>
+        <button class="link">Contact</button>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped lang="scss">
+@use '../styles/mixins';
+
+.nav {
+  position: fixed;
+  top: 2rem;
+  left: 50%;
+  transform: translateX(-50%);
+  mix-blend-mode: difference;
+  z-index: 999;
+
+  .list {
+    display: flex;
+    gap: 2rem;
+    white-space: nowrap;
+
+    .link {
+      @include mixins.link;
+    }
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <main>
+    <NavBar />
+    <HomeHeader />
+  </main>
+</template>
