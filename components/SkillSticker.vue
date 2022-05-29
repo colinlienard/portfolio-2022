80013bf962bef59ebec40b13d629c7e60a57c4a1
Colin Lienard <colin.lienard87@orange.fr>
2022-05-29 19:00:47 +0200

    :dizzy: Add animation for stickers

--- components/SkillSticker.vue
+++ components/SkillSticker.vue
@@ -4,6 +4,9 @@ defineProps<{
   x: number;
   y: number;
 }>();
+
+const maxTilt = 40;
+const tilt = Math.round(Math.random() * maxTilt - maxTilt / 2);
 </script>
 
 <template>
@@ -11,10 +14,33 @@ defineProps<{
 </template>
 
 <style scoped lang="scss">
+@use '../styles/screens';
+@use '../styles/variables';
+
+@keyframes appear {
+  from {
+    opacity: 0;
+    transform: translate(-50%, -50%) rotate(calc(v-bind(tilt) * 1deg))
+      scale(1.2);
+  }
+
+  to {
+    opacity: 1;
+    transform: translate(-50%, -50%) rotate(calc(v-bind(tilt) * 1deg)) scale(1);
+  }
+}
+
 .skill {
   position: absolute;
   top: calc(v-bind(y) * 1px);
   left: calc(v-bind(x) * 1px);
-  transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%) rotate(calc(v-bind(tilt) * 1deg));
+  height: 4rem;
+  pointer-events: none;
+  animation: appear 0.2s variables.$ease-in-out;
+
+  @include screens.laptop {
+    height: 6rem;
+  }
 }
 </style>
