0fa1483f0960395bc9f468fa2651b50d01c5d662
Colin Lienard <colin.lienard87@orange.fr>
2022-06-06 18:04:55 +0200

    :mag: Add a favicon, a title and a meta description

--- App.vue
+++ App.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+// Global meta data
+definePageMeta({
+  link: [
+    {
+      rel: 'favicon',
+      src: '/favicon.ico',
+    },
+  ],
+});
+</script>
+
 <template>
   <NuxtPage />
 </template>
