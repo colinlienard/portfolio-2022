7c0f02188abc35ea3c9143a42e2a7f3a02fbd2ed
ColinLienard <colin.lienard87@orange.fr>
2022-09-22 12:05:11 +0200

    :zap: Use an image for the cube on mobile devices

--- components/CubeBackground.vue
+++ components/CubeBackground.vue
@@ -4,9 +4,10 @@ import { Application } from '@splinetool/runtime';
 const canvas = ref<HTMLCanvasElement>();
 const visible = ref(false);
 const spline = ref<Application>();
+const isMobile = useIsMobile();
 
 onMounted(() => {
-  if (canvas.value) {
+  if (window.innerWidth > 480 && canvas.value) {
     spline.value = new Application(canvas.value);
 
     setTimeout(async () => {
@@ -14,7 +15,13 @@ onMounted(() => {
 
       visible.value = true;
     }, 1000);
+
+    return;
   }
+
+  setTimeout(() => {
+    visible.value = true;
+  }, 1000);
 });
 
 onUnmounted(() => {
@@ -23,25 +30,31 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <canvas ref="canvas" :class="['canvas', { visible }]" />
+  <nuxt-img
+    v-if="isMobile"
+    :class="['image', { visible }]"
+    src="/images/cube.webp"
+    alt=""
+    width="384"
+    height="384"
+    quality="100"
+  />
+  <canvas v-else ref="canvas" :class="['canvas', { visible }]" />
 </template>
 
 <style scoped lang="scss">
+.image {
+  translate: 0 -4rem;
+}
+
+.image,
 .canvas {
   position: absolute;
-  scale: 0.5;
-  translate: 0 -6rem;
-  pointer-events: none;
   transition: opacity 3s variables.$ease-in-out;
+}
 
-  &:not(&.visible) {
-    opacity: 0;
-  }
-
-  @include screens.laptop {
-    scale: 1;
-    translate: 0;
-    pointer-events: all;
-  }
+.image:not(.visible),
+.canvas:not(.visible) {
+  opacity: 0;
 }
 </style>
