10428350d59a017c6a23cbaff24b939ac4c27a5f
Colin Lienard <colin.lienard87@gmail.com>
2023-09-19 20:43:08 +0200

    :sparkles: Add Spotify listening API

--- components/SpotifyListening.vue
+++ components/SpotifyListening.vue
@@ -1,70 +1,15 @@
 <script setup lang="ts">
-/* eslint-disable camelcase */
-const music = ref<{ track: string; artist: string; link: string } | null>();
-const config = useRuntimeConfig();
-let interval: NodeJS.Timer | null = null;
-
-const getMusic = async () => {
-  // Get an access token
-  const responseToken = await fetch(
-    `https://accounts.spotify.com/api/token?grant_type=refresh_token&refresh_token=${config.public.spotifyRefreshToken}`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        Authorization: `Basic ${btoa(
-          `${config.public.spotifyClientId}:${config.public.spotifyClientSecret}`,
-        )}`,
-      },
-    },
-  );
-  if (!responseToken.ok) {
-    return;
-  }
-  const { access_token } = await responseToken.json();
-
-  // Use the access token to fetch data
-  const response = await fetch(
-    'https://api.spotify.com/v1/me/player/currently-playing',
-    {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${access_token}`,
-      },
-    },
-  );
-  if (response.ok) {
-    try {
-      const { item } = await response.json();
-
-      const track = item.name;
-      const artist = item.artists.reduce(
-        (previous: string, current: any) =>
-          `${previous ? `${previous}, ` : ''}${current.name}`,
-        '',
-      );
-      const link = item.external_urls.spotify;
-
-      music.value = {
-        track,
-        artist,
-        link,
-      };
-    } catch {
-      music.value = null;
-    }
-  }
-};
+const { data: music, refresh } = await useFetch('/api/spotify-listening');
+let interval: ReturnType<typeof setInterval> | null = null;
 
 onMounted(() => {
-  getMusic();
-
-  interval = setInterval(getMusic, 60000);
+  interval = setInterval(refresh, 60000);
 });
 
 onUnmounted(() => {
-  clearInterval(interval as NodeJS.Timer);
+  if (interval) {
+    clearInterval(interval);
+  }
 });
 </script>
 
