9e6d39b1ed26da56729053f60bc003029142248a
Colin Lienard <64312634+colinlienard@users.noreply.github.com>
2023-12-14 22:22:58 +0100

    :sparkles: Add cursor party (#81)

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,12 @@ useHead({
         "Hi ! I'm Colin Lienard, a frontend developer passionate about frontend dev. I use React and Nextjs, Vue and Nuxtjs,",
     },
   ],
+  script: [
+    {
+      src: 'https://cursor-party.colinlienard.partykit.dev/cursors.js',
+      tagPosition: 'bodyClose',
+    },
+  ],
 });
 </script>
 
